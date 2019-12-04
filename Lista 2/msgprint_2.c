02bcfeb6d45def32f85400df3b785487867f0022
Lucas <lucascostars@hotmail.com>
2019-12-04 09:21:12 -0300

    Add files via upload

---
+++ Lista 2/msgprint_2.c
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/ipc.h> 
+#include <sys/msg.h> 
+
+key_t key; /* key to be passed to msgget() */ 
+int mqid; /* return value from msgget() */ 
+
+#define EXTERNAL_MQ_ID 10002
+
+
+char msgbuf[100];
+int msgsize;
+
+void main()
+{
+	key = EXTERNAL_MQ_ID;	
+	mqid = msgget(key, 0777);
+	if (mqid == -1) {
+   		printf("msgget falhou\n"); 
+		exit(1); 
+	} 
+	printf("shmget OK, retornou id: %d\n", mqid);
+	printf("vai esperar e imprimir duas mensagens\n");
+
+	msgsize = msgrcv(mqid,msgbuf,sizeof(msgbuf),0,0);
+	if (msgsize<0) {
+		printf("msgrcv falhou\n");
+		exit(1);
+	}
+	printf("recebeu PRIMEIRA mensagem:\n");
+	puts(msgbuf);
+
+	msgsize = msgrcv(mqid,msgbuf,sizeof(msgbuf),0,0);
+	if (msgsize<0) {
+		printf("msgrcv falhou\n");
+		exit(1);
+	}
+	printf("recebeu SEGUNDA mensagem:\n");
+	puts(msgbuf);
+
+	exit(0);	
+}
